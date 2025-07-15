fcc42371d001d500ac439557f5450b4ab976c86f
DystoriaX <62839374+DystoriaX@users.noreply.github.com>
2025-07-15 22:57:43 +0800

    add tmux navigator

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,11 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left" })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right" })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
